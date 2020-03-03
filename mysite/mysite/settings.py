a6923b64f10cae3168bb314fc3fc659cf0fa6f57
martin-martin <breuss.martin@gmail.com>
2020-03-03 18:09:21 +0200

    (pt.2) Register app in settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'polls.apps.PollsConfig',  # or just 'polls'
 ]
 
 MIDDLEWARE = [
